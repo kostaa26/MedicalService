05f61bf015e013cf07b31d571ab2cd5ed0d697e0
QA36 <QA36@DESKTOP-AESHFBQ>
2022-11-28 20:58:19 +0100

    Medical done

---
+++ MedicalService/MedicalService/MedicalTest.cs
@@ -0,0 +1,44 @@
+using MedicalService.Driver;
+using MedicalService.Page;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MedicalService
+{
+    public class Tests
+    {
+        LogInPage logInPage;
+        MedicalPage medicalPage;
+        [SetUp]
+        public void Setup()
+        {
+            WebDrivers.Initialize();
+            logInPage = new LogInPage();
+            medicalPage = new MedicalPage();
+        }
+        [TearDown]
+        public void Close()
+        {
+            WebDrivers.CleanUp();
+
+        }
+
+        [Test]
+        public void TC01_MakeAppointment_ShouldBeCompleted()
+        {
+            logInPage.AppMedic.Click();
+            logInPage.Login("John Doe", "ThisIsNotAPassword");
+
+            medicalPage.SelectOption("Hongkong CURA Healthcare Center");
+            medicalPage.CheckBox.Click();
+            medicalPage.MedicId.Click();
+            medicalPage.Date.SendKeys("25/12/2022");
+            medicalPage.Comment.SendKeys("Gotovo");
+            medicalPage.ButtonApp.Submit();
+
+
+
+            Assert.That("Appointment Confirmation", Is.EqualTo(medicalPage.Confirm.Text));
+                
+        }
+    }
+}
---
+++ MedicalService/MedicalService/Page/LogInPage.cs
@@ -0,0 +1,30 @@
+﻿using MedicalService.Driver;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalService.Page
+{
+    public class LogInPage
+    {
+        private IWebDriver driver = WebDrivers.Instance;
+
+        public IWebElement AppMedic => driver.FindElement(By.Id("btn-make-appointment"));
+
+        public IWebElement UserName => driver.FindElement(By.Id("txt-username"));
+
+        public IWebElement Password => driver.FindElement(By.Id("txt-password"));
+
+        public IWebElement ButtonLogin => driver.FindElement(By.Id("btn-login"));
+        public void Login(string user, string pass)
+        {
+            UserName.SendKeys(user);
+            Password.SendKeys(pass);
+            ButtonLogin.Submit();
+        }
+
+    }
+}
